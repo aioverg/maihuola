3ac4ba7fc5480cee39d76595cfad5d9a5fe65686
aioverg <1010998682@qq.com>
2020-07-03 14:39:45 +0800

    alert checkHistory page

--- pages/task/checkHistory.vue
+++ pages/task/checkHistory.vue
@@ -3,17 +3,20 @@
 		<uni-nav-bar fixed="true" leftIcon="arrowleft" leftText="审核记录"></uni-nav-bar>
 		<view class="task-check-body">
 			<view class="tcb-item" v-for="(item, index) in dataList" :key="index">
-				<view class="tcb-item-one">
-					<view class="tcb-io-title">{{item.title}}</view>
-					<view class="tcb-io-status" :class="item.status == '审核通过' ? 'tcb-io-status-pass' : (item.status == '未通过') ? 'tcb-io-status-fail' : 'tcb-io-status-load' ">
-					    {{item.remark}}{{item.status}}
+				<view class="tcb-item-show">
+					<view class="tcb-item-one">
+						<view class="tcb-io-title">{{item.title}}</view>
+						<view class="tcb-io-status" :class="item.status == '审核通过' ? 'tcb-io-status-pass' : (item.status == '未通过') ? 'tcb-io-status-fail' : 'tcb-io-status-load' ">{{item.status}}</view>
+						<uni-icons v-if="item.status == '未通过'" :type="iconType[index]" @click="showCause(index)" size="10" style="margin: 4px 0 0 4px;"></uni-icons>
+					</view>
+					<view class="tcb-item-two">
+						<view>{{item.time}}</view>
+						<view>{{item.mobile}}</view>
 					</view>
 				</view>
-				<view class="tcb-item-two">
-					<view>{{item.time}}</view>
-					<view>{{item.mobile}}</view>
-				</view>
+				<view class="tcb-item-hidden" v-show="iconType[index] == 'arrowdown'">{{item.remark}}</view>
 			</view>
+
 			<uni-load-more v-if="!aiNoContent" :status="uniLoadMoreStatus"></uni-load-more>
 		</view>
 		<view style="position: fixed; top: 30% ;">
@@ -34,6 +37,7 @@
 				uniLoadMoreStatus: "more",
 				page: 1,
 				lastPage: 1,
+				iconType: []
 			}
 		},
 		onLoad(res) {
@@ -41,12 +45,20 @@
 			this.taskTitle = res.tasktitle
 			this.getDataList()
 		},
-		onReachBottom(){
+		onReachBottom() {
 			this.getDataList()
 		},
 		methods: {
-			getDataList(){
-				if(this.page > this.lastPage){
+			showCause(index){
+				//this.iconType[index] = "arrowdown"
+				if(this.iconType[index] == "arrowup"){
+					this.iconType.splice(index, 1, "arrowdown")
+				}else{
+					this.iconType.splice(index, 1, "arrowup")
+				}
+			},
+			getDataList() {
+				if (this.page > this.lastPage) {
 					this.uniLoadMoreStatus = "noMore"
 					return
 				}
@@ -55,34 +67,33 @@
 					user_id: userId,
 					mission_id: this.taskId,
 					page: this.page,
-				}).then( res => {
-					console.log(res.data.data.total)
-					if(res.data.data.total == 0){
+				}).then(res => {
+					if (res.data.data.total == 0) {
 						this.aiNoContent = true
 						return
-					}else{
+					} else {
 						this.aiNoContent = false
 					}
 					this.page += 1
 					this.lastPage = res.data.data.last_page
-					if(this.page > this.lastPage){
+					if (this.page > this.lastPage) {
 						this.uniLoadMoreStatus = "noMore"
-					}else{
+					} else {
 						this.uniLoadMoreStatus = "more"
 					}
-					for(let item of res.data.data.data){
+					for (let item of res.data.data.data) {
 						item.title = this.taskTitle
-						if(item.status == 0){
+						if (item.status == 0) {
 							item.status = "未通过"
-							item.remark = "（" + item.remark + "）"
 						}
-						if(item.status == 1){
+						if (item.status == 1) {
 							item.status = "审核通过"
 						}
-						if(item.status == 2 || item.status == 3){
+						if (item.status == 2 || item.status == 3) {
 							item.status = "待审核"
 						}
 						this.dataList.push(item)
+						this.iconType.push("arrowup")
 					}
 				})
 			}
@@ -95,38 +106,61 @@
 		width: 750rpx;
 		padding: 10px 30rpx 15px;
 	}
+
 	.tcb-item {
-		box-shadow: 0px 0px 50px 0px rgba(0,0,0,0.06);
-		border-radius: 8px;
 		margin: 0 0 10px;
-		padding: 10px 30rpx;
-		height: 57px;
-		.tcb-item-one {
-			display: flex;
-			justify-content: space-between;
+		width: 690rpx;
+		position: relative;
+		.tcb-item-show {
+			height: 57px;
+			width: 690rpx;
+			padding: 10px 30rpx;
+			border-radius: 8px;
+			box-shadow: 0px 0px 50px 0px rgba(0, 0, 0, 0.06);
+			.tcb-item-one {
+				display: flex;
+				justify-content: space-between;
 			
-			.tcb-io-title {
-				font-size: 13px;
+				.tcb-io-title {
+					font-size: 13px;
+					flex-grow: 1;
+					padding: 0 5px 0 0;
+				}
+			
+				.tcb-io-status {
+					font-size: 15px;
+				}
+			
+				.tcb-io-status-pass {
+					color: #FF716E;
+				}
+			
+				.tcb-io-status-fail {
+					color: #666666;
+				}
+			
+				.tcb-io-status-load {
+					color: #FFA570;
+				}
 			}
-			.tcb-io-status {
-				font-size: 15px;
-			}
-			.tcb-io-status-pass {
-				color: #FF716E;
-			}
-			.tcb-io-status-fail {
-				color: #666666;
-			}
-			.tcb-io-status-load {
-				color: #FFA570;
+			
+			.tcb-item-two {
+				display: flex;
+				justify-content: space-between;
+				margin: 3px 0 0 0;
+				font-size: 12px;
+				color: #999999;
 			}
 		}
-		.tcb-item-two {
-			display: flex;
-			justify-content: space-between;
-			margin: 3px 0 0 0;
-			font-size: 12px;
+		.tcb-item-hidden {
+			width: 650rpx;
+			padding: 6px 30rpx;
+			margin: 0 auto;
+			box-shadow: 0px 0px 50px 0px rgba(0,0,0,0.06);
+			border-radius: 8px;
 			color: #999999;
+			font-size: 12px;
 		}
+		
 	}
 </style>
